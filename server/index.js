358cdb37df2714a349c87953a9db810337be0cd1
Isaiah Santala <bigboyswagcakes@Isaiahs-MacBook-Pro.local>
2019-07-17 10:34:33 -0700

    made small changes to make repo combatible with proxy

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const Schema = require('../db/Schema.js');
 const app = express();
 const PORT = 5152;
+const cors = require('cors')
+
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/dist'));
